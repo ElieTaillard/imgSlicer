39a5e48760ca914da7ba99ab24abc8f8bae13af4
Xaalek <taillardelie@gmail.com>
2021-07-26 16:50:00 +0200

    More generic function

--- Main.py
+++ Main.py
@@ -1,13 +1,13 @@
 import os
 from pathlib import Path
 from chopImg import chopImg
+from PIL import Image
+import uuid
 
 ############ CONSTANTS ############################
 CURRENT_PATH = Path().absolute()
 INPUT_PATH = os.path.join(CURRENT_PATH, "Input")
 OUTPUT_PATH = os.path.join(CURRENT_PATH, "Output")
-
-NUMBER_OF_CHOPS = 4
 ###################################################
 
 
@@ -19,7 +19,11 @@ dirsIN = os.listdir(INPUT_PATH)
 
 for filename in dirsIN:
     if filename.endswith(".png"):
-        chopImg(os.path.join(INPUT_PATH, filename),
-                NUMBER_OF_CHOPS, OUTPUT_PATH)
+        img = Image.open(os.path.join(INPUT_PATH,filename))
+        arrayImg = chopImg(img)
+
+        for img in arrayImg:
+            path = os.path.join(OUTPUT_PATH, str(uuid.uuid4())+".png")
+            img.save(path)
     else:
         continue
--- chopImg.py
+++ chopImg.py
@@ -1,19 +1,14 @@
-import os
-from PIL import Image
-import uuid
+def chopImg(captchaImg):
+    NUMBER_OF_CHOPS = 4
+    arrayImg = []
+    width, height = captchaImg.size
+    chopsize = int(width/NUMBER_OF_CHOPS)
 
-
-def chopImg(infile, numChop, outputPath):
-
-    img = Image.open(infile)
-    width, height = img.size
-    chopsize = int(width/numChop)
-
-    # Save Chops of original image
     for x0 in range(0, width, chopsize):
         for y0 in range(0, height, chopsize):
             box = (x0, y0,
                    x0+chopsize if x0+chopsize < width else width - 1,
                    y0+chopsize if y0+chopsize < height else height - 1)
-            path = os.path.join(outputPath, str(uuid.uuid4())+".png")
-            img.crop(box).save(path)
+            arrayImg.append(captchaImg.crop(box))
+
+    return arrayImg
