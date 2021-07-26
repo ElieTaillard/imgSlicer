b02ddb268ceffdfbb02cabb4752d81d4eddbe982
Xaalek <taillardelie@gmail.com>
2021-07-26 16:51:13 +0200

    print arrayImg

--- Main.py
+++ Main.py
@@ -21,6 +21,7 @@ for filename in dirsIN:
     if filename.endswith(".png"):
         img = Image.open(os.path.join(INPUT_PATH,filename))
         arrayImg = chopImg(img)
+        print(arrayImg)
 
         for img in arrayImg:
             path = os.path.join(OUTPUT_PATH, str(uuid.uuid4())+".png")
